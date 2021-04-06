fda1a9d8aa3a6b306c0983df629473f03ed2ca9a
kisozinov <71232712+kisozinov@users.noreply.github.com>
2021-04-06 13:58:58 +0300

    fix tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -7,7 +7,7 @@
 TEST(train, test1) {
     Train train;
     for (int i = 0; i < 100; ++i) {
-        train.addCage();
+        train.addCage(true);
     }
     EXPECT_EQ(100, train.length());
 }
@@ -15,7 +15,7 @@ TEST(train, test1) {
 TEST(train, test2) {
     Train train;
     for (int i = 0; i < 1; ++i) {
-        train.addCage();
+        train.addCage(false);
     }
     EXPECT_EQ(1, train.length());
 }
@@ -23,5 +23,5 @@ TEST(train, test2) {
 TEST(train, test3) {
     Cage* cage = new Cage();
     cage->on();
-    EXPECT_EQ(true, carriage.get());
+    EXPECT_EQ(true, cage.get());
 }
