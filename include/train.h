00cbf1ffa59ed47a1d9ff50fcf066115022d8dc9
kisozinov <71232712+kisozinov@users.noreply.github.com>
2021-04-06 13:45:23 +0300

    fix train.h

--- include/train.h
+++ include/train.h
@@ -1,38 +1,36 @@
 // Copyright kisozinov 2021
 
-#ifndef INCLUDE_TRAIN_H
-#define INCLUDE_TRAIN_H
+#ifndef INCLUDE_TRAIN_H_
+#define INCLUDE_TRAIN_H_
 
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 
-class Cage
-{
-    bool light; 
-public:
+class Cage {
+    bool light;
+ public:
     Cage* next; 
-    Cage* prev; 
+    Cage* prev;
     Cage() : light(false), next(nullptr), prev(nullptr) {}
-    ~Cage(); 
+    ~Cage();
     void on() { light = true; }
     void off() { light = false; }
     bool get() const { return light; }
     void setLight(bool l) { light = l; }
 };
 
-class Train
-{
+class Train {
  private:
-    Cage* first; 
-    Cage* last;  
+    Cage* first;
+    Cage* last; 
  public:
     Train() : first(nullptr), last(nullptr) {}
     void addCage(bool light);
     void printInfo();
     size_t length();
-    bool getFirst() const { return first; }
-    bool getLast() const { return last; }
+    Cage* getFirst() const { return first; }
+    Cage* getLast() const { return last; }
 };
 
-#endif // INCLUDE_TRAIN_H
+#endif  // INCLUDE_TRAIN_H_
